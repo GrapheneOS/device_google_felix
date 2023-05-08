10c8e193c5237ace8e99f03b43b7cf5793c4af24
Cyan_Hsieh <cyanhsieh@google.com>
2023-05-08 14:54:57 +0800

    Update Felix SVN to 6
    Bug: 278035362
    Change-Id: I724ce2c7605594e1416fdaec12421f62bd613f7e

--- device-felix.mk
+++ device-felix.mk
@@ -197,7 +197,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=5
+    ro.vendor.build.svn=6
 
 # Vibrator HAL
 PRODUCT_VENDOR_PROPERTIES +=\
