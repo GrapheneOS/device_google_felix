eeadf34d45ec2ee7ec653ea0c7b8ab6173755001
Cyan_Hsieh <cyanhsieh@google.com>
2023-05-16 13:15:08 +0800

    Update Felix SVN to 7
    Bug: 278035362
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:1b655e2c5aef87ea5cab78ccddba808fa6f492df)
    Merged-In: Iac3e857c1c1bd2ceda0f111798a18daf869d8e04
    Change-Id: Iac3e857c1c1bd2ceda0f111798a18daf869d8e04

--- device-felix.mk
+++ device-felix.mk
@@ -197,7 +197,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=6
+    ro.vendor.build.svn=7
 
 # Vibrator HAL
 PRODUCT_VENDOR_PROPERTIES +=\
