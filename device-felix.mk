7f4285b673fe9bec56ad4da98c58d8c43ef6ae09
Vince Chiu <vincechiu@google.com>
2023-06-09 10:10:57 +0000

    [Camera] Add CAMERA_MULTI_CAM_STREAMING power hint
    Not limit max CPU frequency when running multicamera streaming on GCA photo mode.
    
    Test: GCA
    Bug: 286792574
    Change-Id: Iecf36827a4f26af4a07b6f8d26cff8e942cd8098

--- device-felix.mk
+++ device-felix.mk
@@ -303,6 +303,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.adjust_backend_min_freq_for_1p_front_video_1080p_30fps=1 \
     persist.vendor.camera.extended_launch_boost=1 \
+    persist.vendor.camera.multicam_streaming_boost=1 \
     persist.vendor.camera.optimized_tnr_freq=1 \
     persist.vendor.camera.raise_buf_allocation_priority=1 \
     persist.vendor.camera.start_cpu_throttling_at_moderate_thermal=1 \
