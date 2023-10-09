4e94c7642a7a2683b96f7fd134b2e7f3ed0aa73e
Roshan Pius <rpius@google.com>
2023-10-09 12:45:26 -0700

    felix: Replace NFC app with NFC apex
    Bug: 303286040
    Test: Compiles
    Merged-In: I9ae8d7630d18075b8620997582761787af1a65b4
    Change-Id: I9ae8d7630d18075b8620997582761787af1a65b4

--- device-felix.mk
+++ device-felix.mk
@@ -92,7 +92,7 @@ PRODUCT_COPY_FILES += \
 	device/google/felix/nfc/libnfc-nci-felix.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf
 
 PRODUCT_PACKAGES += \
-	NfcNci \
+	com.android.nfcservices \
 	Tag \
 	android.hardware.nfc-service.st
 
