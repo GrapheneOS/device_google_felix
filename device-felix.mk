09c1ce320b7948c069bc48645b90eb55cae2bd3e
Roshan Pius <rpius@google.com>
2023-11-04 14:58:26 +0000

    Revert "felix: Replace NFC app with NFC apex"
    Revert submission 24993894-cherrypicker-L80100000963393177:N49200001411943056
    
    Reason for revert: Causing bootloop with signed builds
    
    Reverted changes: /q/submissionid:24993894-cherrypicker-L80100000963393177:N49200001411943056
    
    Bug: 309117462
    Change-Id: Ib09ea604351cea2c4ed40e64c0bf783a666eb08b
    Test: Compiles, device boots up

--- device-felix.mk
+++ device-felix.mk
@@ -98,7 +98,7 @@ PRODUCT_COPY_FILES += \
 	device/google/felix/nfc/libnfc-nci-felix.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf
 
 PRODUCT_PACKAGES += \
-	com.android.nfcservices \
+	NfcNci \
 	Tag \
 	android.hardware.nfc-service.st
 
