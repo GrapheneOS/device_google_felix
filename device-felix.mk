e4b7967a9a4f0643f707ce45997fa50e37d1248d
Ankur Bakshi <ankurbakshi@google.com>
2024-01-26 22:40:53 +0000

    Update felix SVN to 41
    Bug: 313797924
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:8ee4be5ce262b2ff2e3d4378ca2b7cf1a017740c)
    Merged-In: Idc5c9995bde014e20ad6006d97c8b295d22be1b5
    Change-Id: Idc5c9995bde014e20ad6006d97c8b295d22be1b5

--- device-felix.mk
+++ device-felix.mk
@@ -211,7 +211,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=40
+    ro.vendor.build.svn=41
 
 # Vibrator HAL
 PRODUCT_VENDOR_PROPERTIES +=\
