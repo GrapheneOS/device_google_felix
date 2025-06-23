835ac295b7d2ac6a3c5ec00f4fa3234000d978cb
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-23 15:47:03 +0000

    strip location, except for gps conf, modified using TLSv1_2
    cp $ANDROID_BUILD_TOP/vendor/adevtool/dl/unpacked/shiba-BP2A.250605.031.A2/vendor/etc/gnss/gps.xml location/gps_user.6.1.xml
    sed -ri "s:SuplSslMethod=\"SSLv23\":SuplSslMethod=\"TLSv1_2\":g" location/gps_user.6.1.xml

--- device-felix.mk
+++ device-felix.mk
@@ -284,29 +284,8 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.sensor.hinge_angle.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.hinge_angle.xml
 
 # Location
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-    PRODUCT_COPY_FILES += \
-        device/google/felix/location/lhd.conf.f10:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/lhd.conf \
-        device/google/felix/location/scd.conf.f10:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/scd.conf
-    ifneq (,$(filter 6.1, $(TARGET_LINUX_KERNEL_VERSION)))
-        PRODUCT_COPY_FILES += \
-            device/google/felix/location/gps.6.1.xml.f10:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml
-    else
-        PRODUCT_COPY_FILES += \
-            device/google/felix/location/gps.xml.f10:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml
-    endif
-else
-    PRODUCT_COPY_FILES += \
-        device/google/felix/location/lhd_user.conf.f10:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/lhd.conf \
-        device/google/felix/location/scd_user.conf.f10:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/scd.conf
-    ifneq (,$(filter 6.1, $(TARGET_LINUX_KERNEL_VERSION)))
-        PRODUCT_COPY_FILES += \
-            device/google/felix/location/gps_user.6.1.xml.f10:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml
-    else
-        PRODUCT_COPY_FILES += \
-            device/google/felix/location/gps_user.xml.f10:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml
-    endif
-endif
+PRODUCT_COPY_FILES += \
+    device/google/felix/location/gps_user.6.1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml
 
 PRODUCT_PACKAGES += \
         UwbOverlayF10 \
