4d16a5f8f770070aac281c804cad28d4cd61ef18
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-23 15:39:34 +0000

    strip display color data files

--- device-felix.mk
+++ device-felix.mk
@@ -72,13 +72,6 @@ endif
 # Media Performance Class 13
 PRODUCT_PROPERTY_OVERRIDES += ro.odm.build.media_performance_class=33
 
-# Display Config
-PRODUCT_COPY_FILES += \
-	device/google/felix/felix/display_colordata_cal1.pb:$(TARGET_COPY_OUT_VENDOR)/etc/display_colordata_cal1.pb \
-	device/google/felix/felix/display_colordata_dev_cal1.pb:$(TARGET_COPY_OUT_VENDOR)/etc/display_colordata_dev_cal1.pb \
-	device/google/felix/felix/display_golden_cal0.pb:$(TARGET_COPY_OUT_VENDOR)/etc/display_golden_cal0.pb \
-	device/google/felix/felix/display_golden_cal1.pb:$(TARGET_COPY_OUT_VENDOR)/etc/display_golden_cal1.pb
-
 # Display
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	vendor.display.lbe.supported=1 \
