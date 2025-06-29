de0446f4d6bbbf70f75f3866ab4122543493f7c8
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-06-29 15:49:14 +0300

    remove no longer updated overlays

--- device-felix.mk
+++ device-felix.mk
@@ -35,8 +35,6 @@ $(call inherit-product-if-exists, vendor/google_devices/felix/proprietary/Wallpa
 
 $(call inherit-product, device/google/felix/uwb/uwb_calibration_country.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/google/felix/felix/overlay
-
 include device/google/felix/audio/felix/audio-tables.mk
 include device/google/gs201/device-shipping-common.mk
 $(call soong_config_set,fp_hal_feature,pixel_product, product_a)
@@ -86,8 +84,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
 	$(RELEASE_PACKAGE_NFC_STACK) \
 	Tag \
-	android.hardware.nfc-service.st \
-	NfcOverlayFelix
+	android.hardware.nfc-service.st
 
 # Shared Modem Platform
 SHARED_MODEM_PLATFORM_VENDOR := lassen
@@ -158,11 +155,6 @@ FPC_MODULE_TYPE=1542_C
 # Fingerprint config
 include device/google/felix/fingerprint_config.mk
 
-# SKU specific RROs
-PRODUCT_PACKAGES += \
-    SettingsOverlayG0B96 \
-    SettingsOverlayG9FPL
-
 # Trusty liboemcrypto.so
 PRODUCT_SOONG_NAMESPACES += vendor/google_devices/felix/prebuilts
 
@@ -173,10 +165,6 @@ PRODUCT_COPY_FILES += \
 # Location
 PRODUCT_COPY_FILES += \
     device/google/felix/location/gps_user.6.1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml
-
-PRODUCT_PACKAGES += \
-        UwbOverlayF10 \
-        WifiOverlay2023Mid_F10
 
 PRODUCT_SOONG_NAMESPACES += device/google/felix
 
